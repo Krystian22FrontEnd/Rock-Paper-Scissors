6d9a6865c2d2dd5f44c06ab7d912df937b280cdf
Krystian Krupinski <krupinskikrystian01@gmail.com>
2025-02-08 16:03:51 +0100

    added winning, losing and tieing conditions. After closer look i saw that getHumanChoice and getComputerChoice didn't return any actual value, so i added return. Also there is new function getResults, telling us who is a winner.

--- script.js
+++ script.js
@@ -5,36 +5,103 @@ let humanScore = 0;
 
 function getComputerChoice(max) {
     const math = Math.floor(Math.random() * max) + 1
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors"
 
     if (math === 1) {
-        console.log("rock")
+       return (
+        console.log(`Computer's choice %c${rock}`, "font-weight: bold"),
+        rock
+       )
     } else if (math === 2) {
-        console.log("paper")
+        return (
+            console.log(`Computer's choice %c${paper}`, "font-weight: bold"),
+            paper
+           )
     } else if((math === 3)) {
-        console.log("scissors")
+        return (
+            console.log(`Computer's choice %c${scissors}`, "font-weight: bold"),
+            scissors
+           )
     }
 }
-getComputerChoice(3);
 
-function getHumanChoice() {
+
+function getHumanChoice () {
     
    let choice = prompt("Choose").toLowerCase();
         
    if(choice !== "rock" && choice !== "paper" && choice !== "scissors") {
     console.log("Please enter a valid name")
    } else {
-    console.log(choice)
+    return choice
    }
     }
-getHumanChoice();
 
-function playRound(humanChoice, computerChoice) {
+    function getResults () {
+        if(humanScore > computerScore){
+            return alert(`You are a winner! With ${humanScore} point/s!!!`)
+        } else if (computerScore > humanScore){
+            return alert(`Computer is a winner! With ${computerScore} point/s!!!`)
+        } else {
+            return alert (`We have a tie! With ${humanScore} point/s and ${computerScore} point/s on each side!!!`)
+        }
+    }
+    
+function playGame () {
+    function playRound(humanChoice, computerChoice) {
 
+        if(humanChoice === "rock" && computerChoice === "scissors") {
+               return (
+                    console.log("Congrats! You won!"),
+                    console.log("Your score:",  ++humanScore)
+               ) 
+        
+        } else if (humanChoice === "rock" && computerChoice === "rock") {
+              return  console.log("Tie!")
+        
+        } else if (humanChoice === "rock" && computerChoice === "paper"){
+                return (
+                    console.log("You lose!"),
+                    console.log("Computer score:",  ++computerScore)
+                )
+
+        } else if (humanChoice === "paper" && computerChoice === "rock"){
+                return (
+                    console.log("Congrats! You won!"),
+                    console.log("Your score:",  ++humanScore)
+                )
+            
+        } else if (humanChoice === "paper" && computerChoice === "paper") {
+                return console.log("Tie!")
+
+        }  else if (humanChoice === "paper" && computerChoice === "scissors"){
+                return (
+                    console.log("You lose!"),
+                    console.log("Computer score:",  ++computerScore))
+
+        } else if (humanChoice === "scissors" && computerChoice === "paper"){
+                return (
+                    console.log("Congrats! You won!"),
+                    console.log("Your score:",  ++humanScore))
+        
+        } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+                return console.log("Tie!")
+
+        }  else if (humanChoice === "scissors" && computerChoice === "rock"){
+                return (
+                    console.log("You lose!"),
+                    console.log("Computer score:",  ++computerScore)
+            )
 }
+        }
 
-function playGame() {
-    const humanSelection = humanChoice();
-    const computerSelection = computerChoice();
-
-playRound(humanSelection, computerSelection);
+        playRound(getHumanChoice(), getComputerChoice(3));
+        playRound(getHumanChoice(), getComputerChoice(3));
+        playRound(getHumanChoice(), getComputerChoice(3));
+        playRound(getHumanChoice(), getComputerChoice(3));
+        playRound(getHumanChoice(), getComputerChoice(3));
+        getResults();
 }
+playGame()
